73d4ccad966271abddac63ba551bfba0749b134e
Akmal Maulana Azzam <akmalazzam2240@gmail.com>
2018-10-13 07:13:34 +0700

    Adding previous and next button

--- view/src/windows/MainWindow/specific-component/PlayerController/component/index.js
+++ view/src/windows/MainWindow/specific-component/PlayerController/component/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import {
+  faPlay,
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 import Touchable from '../../../../../common/Touchable';
 
@@ -12,6 +16,23 @@ function PlayerControllerComponent({ playFunction }) {
         className="player-controller__big-play-button"
         onClick={playFunction}
       />
+      <div className="controller">
+        <Touchable
+          icon={faAngleDoubleLeft}
+          className="player-controller__prev-button"
+          onClick={playFunction}
+        />
+        <Touchable
+          icon={faPlay}
+          className="player-controller__play-button"
+          onClick={playFunction}
+        />
+        <Touchable
+          icon={faAngleDoubleRight}
+          className="player-controller__next-button"
+          onClick={playFunction}
+        />
+      </div>
     </div>
   );
 }
